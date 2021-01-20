4fb4d9b701bf39e701d7b75fa6b8b3978615fb65
ari-pekka jääskeläinen <ari.jaaskelainen1@gmail.com>
2021-01-20 10:09:01 +0200

    refactor matches view search section

--- src/views/matchesView/MatchesView.tsx
+++ src/views/matchesView/MatchesView.tsx
@@ -1,19 +1,16 @@
-import React, { FormEvent } from "react";
+import React from "react";
 import styled from "styled-components";
 import { LogInPrimaryHeadline } from "../logIn/LogIn";
 import { Process, Status, SecondaryHeadline } from "../../App";
 import { ProfileBall } from "../../components/profileBall/ProfileBall";
 import { HeartIcon } from "../../components/icons/HeartIcon";
-import { Form, InputField } from "../logIn/LogIn";
 import { Puff } from "../../components/puff/Puff";
-import { getUser } from "../../apiService/getUser";
 import { TransparentButton, Mark } from "../accountSettings/AccountSettings";
-import { pairWithUser } from "../../apiService/pairWithUser";
-import { SearchIcon } from "../../components/icons/SearchIcon";
 import { rejectIncomingRequest } from "../../apiService/rejectIncomingRequest";
 import { acceptIncomingRequest } from "../../apiService/acceptIncomingRequest";
 import { AnimateType } from "../../components/icons/HeartIcon";
 import { DetailsSection } from "./detailsSection/DetailsSection";
+import { SearchSection } from "./searchSection/SearchSection";
 
 type Props = {
   getCurrentAuthenticatedUserProcess: Process;
@@ -25,15 +22,6 @@ type Props = {
 };
 
 export const MatchesView = (props: Props) => {
-  const [searchProcess, setSearchProcess] = React.useState<Process>({
-    status: Status.INITIAL,
-  });
-  const [searchFieldValue, setSearchFieldValue] = React.useState<string>("");
-
-  const [pairingProcess, setPairingProcess] = React.useState<Process>({
-    status: Status.INITIAL,
-  });
-
   const [
     rejectIncomingRequestProcess,
     setRejectIncomingRequestProcess,
@@ -43,61 +31,6 @@ export const MatchesView = (props: Props) => {
     acceptIncomingRequestProcess,
     setAcceptIncomingRequestProcess,
   ] = React.useState<Process>({ status: Status.INITIAL });
-
-  const searchUser = async (event: FormEvent) => {
-    event.preventDefault();
-    if (
-      props.getCurrentAuthenticatedUserProcess.status === Status.SUCCESS &&
-      props.getCurrentSessionProcess.status === Status.SUCCESS &&
-      searchFieldValue.length > 0
-    ) {
-      try {
-        setSearchProcess({ status: Status.LOADING });
-        const searchForUserResponse = await getUser(
-          searchFieldValue,
-          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
-        );
-        setSearchProcess({
-          status: Status.SUCCESS,
-          data: searchForUserResponse,
-        });
-        setSearchFieldValue("");
-      } catch (searchForUserError) {
-        setSearchFieldValue("");
-        setSearchProcess({ status: Status.ERROR, error: searchForUserError });
-        alert(`did not find user "${searchFieldValue}"`);
-      }
-    } else {
-      alert("form not complete");
-    }
-  };
-
-  const pairWith = async () => {
-    if (
-      searchProcess.status === Status.SUCCESS &&
-      props.getCurrentSessionProcess.status === Status.SUCCESS &&
-      props.getUserItemProcess.status === Status.SUCCESS
-    ) {
-      try {
-        setSearchProcess({ status: Status.INITIAL });
-        setPairingProcess({ status: Status.LOADING });
-        const pairWithUserResponse = await pairWithUser(
-          searchProcess.data.username.S,
-          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
-        );
-        setPairingProcess({
-          status: Status.SUCCESS,
-          data: pairWithUserResponse,
-        });
-        props.getUserItem(
-          props.getUserItemProcess.data.username.S,
-          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
-        );
-      } catch (pairWithUserError) {
-        setPairingProcess({ status: Status.ERROR, error: pairWithUserError });
-      }
-    }
-  };
 
   const rejectIncoming = async (rejectUsername: string) => {
     if (
@@ -253,77 +186,17 @@ export const MatchesView = (props: Props) => {
               getUserItem={props.getUserItem}
               getPairedUser={props.getPairedUser}
             />
-            {!props.getUserItemProcess.data.outgoingRequests && !isPartnered && (
-              <MatchSectionWrapper>
-                <SecondaryHeadline>Search</SecondaryHeadline>
-                <FormWrapper>
-                  <Form onSubmit={searchUser}>
-                    <InputFieldWrapper>
-                      <InputField
-                        type="text"
-                        value={searchFieldValue}
-                        onChange={(event) =>
-                          setSearchFieldValue(event.target.value)
-                        }
-                        placeholder="search for a partner"
-                      />
-                      <SearchIconButton title="search" onClick={searchUser}>
-                        <SearchIcon size={20} animate />
-                      </SearchIconButton>
-                    </InputFieldWrapper>
-                  </Form>
-                </FormWrapper>
-                {searchProcess.status === Status.LOADING && (
-                  <Puff size={50} fill="lightblue" />
-                )}
-                {searchProcess.status === Status.SUCCESS && (
-                  <FoundUserWrapper>
-                    <ProfileWrapper>
-                      <ProfileBall
-                        firstName={searchProcess.data.username.S}
-                        image={
-                          searchProcess.data.profilePicture
-                            ? `https://couplesmoviepickerbacken-profilepicturesbucketa8b-2miadmkpd2b7.s3.eu-central-1.amazonaws.com/${searchProcess.data.profilePicture.S}`
-                            : undefined
-                        }
-                        isCurrentUser={false}
-                        size={40}
-                        animate={false}
-                        fontSize={25}
-                        showText
-                        shadow={false}
-                        border={false}
-                      />
-                      <ProfileText isPartnered={searchProcess.data.partner}>
-                        {searchProcess.data.partner
-                          ? `${searchProcess.data.username.S} is already partnered with ${searchProcess.data.partner.S}`
-                          : searchProcess.data.username.S}
-                      </ProfileText>
-                    </ProfileWrapper>
-                    {!searchProcess.data.partner && (
-                      <ButtonsWrapper>
-                        {pairingProcess.status === Status.INITIAL && (
-                          <TransparentButton
-                            onClick={() => pairWith()}
-                            title={`pair with ${searchProcess.data.username.S}`}
-                          >
-                            <HeartIcon
-                              size={30}
-                              animate={AnimateType.COLOR}
-                              isRed={false}
-                            />
-                          </TransparentButton>
-                        )}
-                        {pairingProcess.status === Status.LOADING && (
-                          <Puff size={20} fill="lightblue" />
-                        )}
-                        {pairingProcess.status === Status.SUCCESS && <div />}
-                      </ButtonsWrapper>
-                    )}
-                  </FoundUserWrapper>
-                )}
-              </MatchSectionWrapper>
-            )}
+            {!props.getUserItemProcess.data.outgoingRequests &&
+              !isPartnered && (
+                <SearchSection
+                  getCurrentAuthenticatedUserProcess={
+                    props.getCurrentAuthenticatedUserProcess
+                  }
+                  getCurrentSessionProcess={props.getCurrentSessionProcess}
+                  getUserItemProcess={props.getUserItemProcess}
+                  getUserItem={props.getUserItem}
+                />
+              )}
             {props.getUserItemProcess.data.incomingRequests && (
               <SectionWrapper>
                 <SecondaryHeadline>Requests</SecondaryHeadline>
@@ -346,12 +219,7 @@ export const MatchSectionWrapper = styled.div`
   text-align: start;
 `;
 
-const FormWrapper = styled.div`
-  width: 100%;
-  margin-top: 20px;
-`;
-
-const FoundUserWrapper = styled.div`
+export const FoundUserWrapper = styled.div`
   display: flex;
   align-items: center;
   margin-top: 20px;
@@ -365,7 +233,7 @@ type ProfileTextProps = {
   isPartnered?: boolean;
 };
 
-const ProfileText = styled.p`
+export const ProfileText = styled.p`
   margin: 0;
   margin-left: 20px;
   color: ${(props: ProfileTextProps) => (props.isPartnered ? "red" : "black")};
@@ -373,25 +241,14 @@ const ProfileText = styled.p`
     props.isPartnered ? "12px" : "16px"};
 `;
 
-const ProfileWrapper = styled.div`
+export const ProfileWrapper = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const ButtonsWrapper = styled.div`
+export const ButtonsWrapper = styled.div`
   margin-left: 20px;
   display: flex;
-`;
-
-const InputFieldWrapper = styled.div`
-  position: relative;
-  width: 250px;
-`;
-
-const SearchIconButton = styled(TransparentButton)`
-  position: absolute;
-  top: 0;
-  right: 0;
 `;
 
 const SectionWrapper = styled.div`
---
+++ src/views/matchesView/searchSection/SearchSection.tsx
@@ -0,0 +1,175 @@
+import React, { FormEvent } from "react";
+import styled from "styled-components";
+import { Process, SecondaryHeadline, Status } from "../../../App";
+import { SearchIcon } from "../../../components/icons/SearchIcon";
+import { Form, InputField } from "../../logIn/LogIn";
+import { getUser } from "../../../apiService/getUser";
+import { pairWithUser } from "../../../apiService/pairWithUser";
+import { Puff } from "../../../components/puff/Puff";
+import { TransparentButton } from "../../accountSettings/AccountSettings";
+import {
+  FoundUserWrapper,
+  MatchSectionWrapper,
+  ProfileWrapper,
+  ProfileText,
+  ButtonsWrapper
+} from "../MatchesView";
+import { ProfileBall } from "../../../components/profileBall/ProfileBall";
+import { AnimateType, HeartIcon } from "../../../components/icons/HeartIcon";
+
+type Props = {
+  getCurrentAuthenticatedUserProcess: Process;
+  getCurrentSessionProcess: Process;
+  getUserItemProcess: Process;
+  getUserItem: (username: string, jwtToken: string) => void;
+};
+
+export const SearchSection = (props: Props) => {
+  const [searchProcess, setSearchProcess] = React.useState<Process>({
+    status: Status.INITIAL,
+  });
+  const [searchFieldValue, setSearchFieldValue] = React.useState<string>("");
+
+  const [pairingProcess, setPairingProcess] = React.useState<Process>({
+    status: Status.INITIAL,
+  });
+  const searchUser = async (event: FormEvent) => {
+    event.preventDefault();
+    if (
+      props.getCurrentAuthenticatedUserProcess.status === Status.SUCCESS &&
+      props.getCurrentSessionProcess.status === Status.SUCCESS &&
+      searchFieldValue.length > 0
+    ) {
+      try {
+        setSearchProcess({ status: Status.LOADING });
+        const searchForUserResponse = await getUser(
+          searchFieldValue,
+          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
+        );
+        setSearchProcess({
+          status: Status.SUCCESS,
+          data: searchForUserResponse,
+        });
+        setSearchFieldValue("");
+      } catch (searchForUserError) {
+        setSearchFieldValue("");
+        setSearchProcess({ status: Status.ERROR, error: searchForUserError });
+        alert(`did not find user "${searchFieldValue}"`);
+      }
+    } else {
+      alert("form not complete");
+    }
+  };
+
+  const pairWith = async () => {
+    if (
+      searchProcess.status === Status.SUCCESS &&
+      props.getCurrentSessionProcess.status === Status.SUCCESS &&
+      props.getUserItemProcess.status === Status.SUCCESS
+    ) {
+      try {
+        setSearchProcess({ status: Status.INITIAL });
+        setPairingProcess({ status: Status.LOADING });
+        const pairWithUserResponse = await pairWithUser(
+          searchProcess.data.username.S,
+          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
+        );
+        setPairingProcess({
+          status: Status.SUCCESS,
+          data: pairWithUserResponse,
+        });
+        props.getUserItem(
+          props.getUserItemProcess.data.username.S,
+          props.getCurrentSessionProcess.data.getIdToken().getJwtToken()
+        );
+      } catch (pairWithUserError) {
+        setPairingProcess({ status: Status.ERROR, error: pairWithUserError });
+      }
+    }
+  };
+  return (
+    <MatchSectionWrapper>
+      <SecondaryHeadline>Search</SecondaryHeadline>
+      <FormWrapper>
+        <Form onSubmit={searchUser}>
+          <InputFieldWrapper>
+            <InputField
+              type="text"
+              value={searchFieldValue}
+              onChange={(event) => setSearchFieldValue(event.target.value)}
+              placeholder="search for a partner"
+            />
+            <SearchIconButton title="search" onClick={searchUser}>
+              <SearchIcon size={20} animate />
+            </SearchIconButton>
+          </InputFieldWrapper>
+        </Form>
+      </FormWrapper>
+      {searchProcess.status === Status.LOADING && (
+        <Puff size={50} fill="lightblue" />
+      )}
+      {searchProcess.status === Status.SUCCESS && (
+        <FoundUserWrapper>
+          <ProfileWrapper>
+            <ProfileBall
+              firstName={searchProcess.data.username.S}
+              image={
+                searchProcess.data.profilePicture
+                  ? `https://couplesmoviepickerbacken-profilepicturesbucketa8b-2miadmkpd2b7.s3.eu-central-1.amazonaws.com/${searchProcess.data.profilePicture.S}`
+                  : undefined
+              }
+              isCurrentUser={false}
+              size={40}
+              animate={false}
+              fontSize={25}
+              showText
+              shadow={false}
+              border={false}
+            />
+            <ProfileText isPartnered={searchProcess.data.partner}>
+              {searchProcess.data.partner
+                ? `${searchProcess.data.username.S} is already partnered with ${searchProcess.data.partner.S}`
+                : searchProcess.data.username.S}
+            </ProfileText>
+          </ProfileWrapper>
+          {!searchProcess.data.partner && (
+            <ButtonsWrapper>
+              {pairingProcess.status === Status.INITIAL && (
+                <TransparentButton
+                  onClick={() => pairWith()}
+                  title={`pair with ${searchProcess.data.username.S}`}
+                >
+                  <HeartIcon
+                    size={30}
+                    animate={AnimateType.COLOR}
+                    isRed={false}
+                  />
+                </TransparentButton>
+              )}
+              {pairingProcess.status === Status.LOADING && (
+                <Puff size={20} fill="lightblue" />
+              )}
+              {pairingProcess.status === Status.SUCCESS && <div />}
+            </ButtonsWrapper>
+          )}
+        </FoundUserWrapper>
+      )}
+    </MatchSectionWrapper>
+  );
+};
+
+const FormWrapper = styled.div`
+  width: 100%;
+  margin-top: 20px;
+`;
+
+const InputFieldWrapper = styled.div`
+  position: relative;
+  width: 250px;
+`;
+
+const SearchIconButton = styled(TransparentButton)`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;

decec6428fe03a132a936cffa0d4e70aefa8b412
ari-pekka jääskeläinen <ari.jaaskelainen1@gmail.com>
2021-01-24 21:15:17 +0200

    update movie view

--- src/views/movieView/MovieView.tsx
+++ src/views/movieView/MovieView.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { Process, Status } from "../../App";
-import { sizingScale } from "../../styles/Variables";
+import { fontSizes, sizingScale } from "../../styles/Variables";
 import { getMovieDetails } from "../../apiService/getMovieDetails";
 import { useParams } from "react-router-dom";
-import { PrimaryHeadline } from "../../styles/Styles";
+import {
+  PrimaryHeadline,
+  SecondaryHeadline,
+  TertiaryHeadline,
+} from "../../styles/Styles";
 import { CardContentWrapper } from "../logIn/LogIn";
 
 type Props = {
@@ -53,6 +57,12 @@ export const MovieView = (props: Props) => {
     }
   }, [props.getUserItemProcess.status]);
 
+  // const getGenresList = (genres: any[]) => {
+  //   return genres.map((genre: any) => {
+  //     <
+  //   })
+  // }
+
   console.log(getMovieDetailsProcess, "getMovieDetailsProcess");
   if (
     getMovieDetailsProcess.status === Status.SUCCESS &&
@@ -70,6 +80,36 @@ export const MovieView = (props: Props) => {
           <PrimaryHeadline>
             {getMovieDetailsProcess.data.original_title}
           </PrimaryHeadline>
+          <InfoList>
+            <InfoListItem>
+              <InfoListItemText>
+                {`${getMovieDetailsProcess.data.runtime}min`}
+              </InfoListItemText>
+            </InfoListItem>
+            <InfoListItem>
+              <InfoListItemText>
+                {getMovieDetailsProcess.data.genres
+                  .map((genre: any) => genre.name)
+                  .join(", ")}
+              </InfoListItemText>
+            </InfoListItem>
+            <InfoListItem>
+              <InfoListItemText>
+                {getMovieDetailsProcess.data.release_date}
+              </InfoListItemText>
+            </InfoListItem>
+          </InfoList>
+          <MovieViewSection>
+            <SecondaryHeadline>Overview</SecondaryHeadline>
+            <Text>{getMovieDetailsProcess.data.overview}</Text>
+          </MovieViewSection>
+          <MovieViewSection>
+            <SecondaryHeadline>Details</SecondaryHeadline>
+            <DetailItemWrapper>
+              <MovieTertiaryHeadline>duration</MovieTertiaryHeadline>
+              <Text>{getMovieDetailsProcess.data.runtime}</Text>
+            </DetailItemWrapper>
+          </MovieViewSection>
         </MovieViewCardContentWrapper>
       </Wrapper>
     );
@@ -77,6 +117,11 @@ export const MovieView = (props: Props) => {
     return <div />;
   }
 };
+
+const MovieViewSection = styled.div`
+  text-align: start;
+  margin-top: ${`${sizingScale[6]}px`};
+`;
 
 const MovieViewCardContentWrapper = styled(CardContentWrapper)`
   margin-top: ${`${sizingScale[4]}px`};
@@ -94,4 +139,37 @@ const ImageSection = styled.div`
 
 const Image = styled.img`
   width: ${`${sizingScale[13]}px`};
+`;
+
+const DetailItemWrapper = styled.div`
+  margin-top: 10px;
+  display: flex;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  font-size: ${`${fontSizes[0]}px`};
+  font-weight: 300;
+  margin-top: ${`${sizingScale[2]}px`};
+`;
+
+const InfoListItemText = styled.h6`
+  margin: 0;
+  font-size: ${`${fontSizes[0]}px`};
+  font-weight: 400;
+  color: gray;
+`;
+
+const MovieTertiaryHeadline = styled(TertiaryHeadline)`
+  color: gray;
+`;
+
+const InfoList = styled.ul`
+  list-style-type: none;
+  display: flex;
+`;
+
+const InfoListItem = styled.li`
+  padding: 0;
+  margin: 0 ${`${sizingScale[1]}px`};
 `;
